432863bfb68adebaa063f3ca7bcf3502b710f052
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-08-21 08:48:00 +0900

    init project

---
+++ src/test/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,22 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 할인에 대한건 나는 모르겠어. 할인은 너가 알아서 정해서 알려줘.
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
